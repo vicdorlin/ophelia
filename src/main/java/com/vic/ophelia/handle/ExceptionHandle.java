d5b63105a7e6a9af05eced2c87ed5956497b5de9
linss <linss@aixuedai.com>
2018-03-02 10:57:26 +0800

    .

--- src/main/java/com/vic/ophelia/handle/ExceptionHandle.java
+++ src/main/java/com/vic/ophelia/handle/ExceptionHandle.java
@@ -3,20 +3,25 @@ package com.vic.ophelia.handle;
 import com.vic.ophelia.domain.ResultVo;
 import com.vic.ophelia.exception.GlobalException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * @author vicdor
  * @create 2018-02-07 15:43
  */
-@ControllerAdvice
+//@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class ExceptionHandle {
 
+    @ExceptionHandler(GlobalException.class)
+    public ResultVo handleGlobalException(GlobalException exception) {
+        return ResultVo.fail(exception.getCode(), exception.getMessage());
+    }
+
     @ExceptionHandler
-    @ResponseBody
+//    @ResponseBody
     public ResultVo handle(Exception e) {
         if (e instanceof GlobalException) {
             GlobalException exception = (GlobalException) e;
